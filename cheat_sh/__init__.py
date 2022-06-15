622564879a96125af84b83c26aefb253b50bf184
Xavier Mitault <xavier.mitault@epitech.eu>
2022-06-15 17:40:23 +0000

    Add option to not add syntax color

--- cheat_sh/__init__.py
+++ cheat_sh/__init__.py
@@ -1,9 +1,10 @@
 import requests
+import re
 from urllib.parse import quote_plus
 
 def get_base_url() -> str:
-    """Return http://http://cheat.sh/."""
-    base_url = "http://http://cheat.sh/"
+    """Return http://cheat.sh/."""
+    base_url = "http://cheat.sh/"
     return base_url
 
 def get_supported_language() -> list[str]:
@@ -24,22 +25,31 @@ def get_supported_language() -> list[str]:
     ]
     return langues
 
-def requests_cheat_sh(request: str) -> str:
+def requests_cheat_sh(request: str, color: bool = False) -> str:
     """Get directly the request
     Just add base_url and encode the all.
+    :param color: color escape sequencies to get color in terminal (but not very helpfull otherwise)
     """
     url = get_base_url()
     url += quote_plus(request, safe='/')
-    result = requests.request(method="GET", url=url)
+    if color == False:
+        url += "?style=bw"
+    result = requests.get(url)
     if result.ok == False:
         return ""
-    return result.text
+    text = result.text
+    if color == False:
+        exit_color = chr(0x1b)
+        text = re.sub(exit_color + r"\[[0-9]{2}(;[0-9]{2}){0,1}m", "", text)
+    return text
 
-def ask(question: str, language: str = "") -> str:
+def ask(question: str, language: str = "", color: bool = False) -> str:
     """Ask a question to cheat.sh.
 
     You can provide a language, or if the first word is a supported language,
     it will fill it automaticaly.
+
+    :param color: color escape sequencies to get color in terminal (but not very helpfull otherwise)
 
     Return all the text returned by cheat.sh.
     """
@@ -54,13 +64,16 @@ def ask(question: str, language: str = "") -> str:
     if language != "":
         url += quote_plus(language, safe="") + "/"
     url = quote_plus(question, safe='') + "/"
-    result = requests_cheat_sh(url)
+    result = requests_cheat_sh(url, color=color)
     return result
 
-def learn(language: str) -> str:
-    """Get help to learn a language."""
+def learn(language: str, color: bool = False) -> str:
+    """Get help to learn a language.
+
+    :param color: color escape sequencies to get color in terminal (but not very helpfull otherwise)
+    """
     if language not in get_supported_language():
         return ""
     url = quote_plus(language, safe='') + "/:learn"
-    result = requests_cheat_sh(url)
+    result = requests_cheat_sh(url, color=color)
     return result
